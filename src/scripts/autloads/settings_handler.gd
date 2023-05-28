736a0e3d6f63fe87b8c5647f7d9a6fb9f5e5b689
voylin <0voylin0@gmail.com>
2023-05-28 17:14:45 +0900

    Setting handler update

--- src/scripts/autloads/settings_handler.gd
+++ src/scripts/autloads/settings_handler.gd
@@ -1,28 +1,27 @@
 extends Node
 
 const settings_path := "user://settings.dat"
-const folder_structure: PackedStringArray = [
-	"modules",
-	"modules/project_scene",
-]
 
+# Save-able data
 var projects := [] # List of all saved project paths
 var sel_modules := {
-	"ProjectManager": "default"
+	"project_manager": "default"
 }
 
 
 func _ready() -> void:
 	if FileAccess.file_exists(settings_path): 
 		load_settings()
-		return
-	save_settings()
+	else:
+		save_settings()
+	
+	_update_module_tree()
 
 
 func save_settings() -> void:
-	print("Save settings ...")
 	var data := {
-		"projects": projects
+		"projects": projects,
+		"sel_modules": sel_modules
 	}
 	var file := FileAccess.open_compressed(settings_path, FileAccess.WRITE)
 	file.store_var(data)
@@ -30,9 +29,18 @@ func save_settings() -> void:
 
 
 func load_settings() -> void:
-	print("Loading settings ...")
 	var file := FileAccess.open_compressed(settings_path, FileAccess.READ)
 	var temp_data: Dictionary = file.get_var()
 	file.close()
 	for x in temp_data:
 		if get(x) != null: set(x, temp_data[x])
+
+
+func _update_module_tree() -> void:
+	# Updating the module paths in user://
+	var dir := DirAccess.open("user://")
+	if !dir.dir_exists("modules"): dir.make_dir("modules")
+	dir.change_dir("modules")
+	for module_dir in DirAccess.get_directories_at("res://modules"):
+		if !dir.dir_exists(module_dir):
+			dir.make_dir(module_dir)
