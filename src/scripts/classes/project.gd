99dea860e3b905aba27456234077d089c5241782
voylin <0voylin0@gmail.com>
2023-07-20 12:16:32 +0900

    Updated Project  class

--- src/scripts/classes/project.gd
+++ src/scripts/classes/project.gd
@@ -1,85 +1,60 @@
-class_name Project
-extends Node
-
-signal update_files_interface_tree
+class_name Project extends Node
 
 
-var p_name: String = "Missing project file"
-var p_path: String
-var p_creation: int = 0
-var p_edit: int = 0
+var project_name: String = "Missing"
+var project_path: String = "/home/"
+var project_creation: int = 0
+var project_edit: int = 0
+var project_fps: int
 
-var folders := {
-	"Main": []
-}
+var files := {} # video id's start with v_, pictures = p_, audio = a_, color = c_
+var folders := {"Main": []}
 
-
-## Get only the essential data for using inside of the project manager
-static func get_project_manager_data(project_path: String) -> Dictionary:
-	var project_dic := {
-		"p_name": "Missing project file",
-		"p_path": project_path,
-		"p_creation": 0,
-		"p_edit": 0 }
-	if !FileAccess.file_exists(project_path): return project_dic
-	var project_file := FileAccess.open_compressed(project_path, FileAccess.READ)
-	var error := FileAccess.get_open_error()
-	if error:
-		printerr("Could not open project file at %s.\n\tError: %s" % [project_path, error])
-		return project_dic
-	var project_data: Dictionary = project_file.get_var()
-	project_dic.p_name = project_data.p_name
-	project_dic.p_creation = project_data.p_creation
-	project_dic.p_edit = project_data.p_edit
-	return project_dic
+var lines := [[],[]] # First array is for video, second array for audio
+# TODO: Timeline stuff
+#var blocks := {} # group_id: [start_frame, end_frame]
+#var groups := {} # group_id: [block_id's,...]
+#var effects := {}
+# Every line is a dictionary.
+# The key is the frame, this frame has an array
+# [file_id, video_frame, block_id, group_id]
+# Effects get added by looking at block id
+# videos should be saved with an id number, this id number gets put inside
+# of the folders. The id should contain an array which contains the properties
+# like fps, max frames, path, ...
 
 
-## Creates a new project at the desired location
-static func create_new_project(project_name: String, project_folder: String) -> Dictionary:
-	if DirAccess.dir_exists_absolute(project_folder):
-		printerr("Folder does not exist at path: %s" % project_folder)
-		return {}
-	var file_correct_name: String = "" 
-	var invalid_characters := ['<','>',':','"','/','\\','|','?','*']
-	for character in project_name:
-		file_correct_name += "_" if character in invalid_characters else character
-	var project := Project.new()
-	project.p_name = project_name
-	project.p_path = "%s/%s.gozen" % [project_folder, file_correct_name]
-	project.p_creation = TimeManager.date_to_int()
-	project.p_edit = project.p_creation
-	project.save_project()
-	return get_project_manager_data(project.p_path)
-
-
-## Saves the project data to the correct file
 func save_project() -> void:
 	var data := {}
-	data["p_name"] = p_name
-	data["p_path"] = p_path
-	data["p_creation"] = p_creation
-	data["p_edit"] = p_edit
-	var file := FileAccess.open_compressed(p_path, FileAccess.WRITE)
+	for prop in get_property_list():
+		if prop.usage == 4096: data[prop.name] = get(prop.name)
+	
+	var file := FileAccess.open_compressed(project_path, FileAccess.WRITE)
 	file.store_var(data)
-	file.close()
 
 
-func load_project(project_path: String) -> void:
-	p_path = project_path # In case the saved path has changed
-	if !FileAccess.file_exists(project_path):
-		return
+func load_project(path: String) -> void:
+	project_path = path # Incase original path changed
+	
+	if !FileAccess.file_exists(project_path): return
+	
 	var file := FileAccess.open_compressed(project_path, FileAccess.READ)
-	var project_data = file.get_var()
-	file.close()
-	
-	p_name = project_data.p_name
-	p_creation = project_data.p_creation
-	p_edit = project_data.p_edit
-#	p_files_id = project_data.p_files_id
-	
-#	for project_file in project_data.p_files:
-#		var new_file: File = File.new()
-#		new_file.load_data(project_file)
-#		p_files[new_file.f_id] = new_file
+	var data: Dictionary = file.get_var()
+	for prop in data: set(prop, data[prop])
 
 
+func new_project(p_name: String, p_folder: String, p_fps: int = 30) -> void:
+	if !DirAccess.dir_exists_absolute(p_folder):
+		return printerr("Folder does not exist at path: %s" % p_folder)
+	
+	project_name = p_name
+	project_path = "%s/%s.gozen" % [p_folder, p_name]	
+	project_creation = TimeManager.date_to_int()
+	project_edit = project_creation
+	project_fps = p_fps
+	
+	# Fix path to not include invalid characters
+	for x in ['<','>',':','"','/','\\','|','?','*',' ']:
+		project_path = project_path.replace(x,"_")
+	
+	save_project()
