3ba83b09ded05e76938cc48cfcd9c3ab0caa221a
voylin <0voylin0@gmail.com>
2023-07-22 14:47:30 +0900

    Change to autoloads

---
+++ src/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Editor
+signal start_editing(project_info)
+
+# StartUp window
+signal show_startup_panel
+signal update_project_list
+signal add_projects_list_entry(project)
+
+# NewProject window
+signal show_new_project_panel
+
+
+const VERSION := "0.0.1-Alpha"
+
+var project : Project
--- src/scripts/autoloads/thumb_manager.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-# Save all thumbs to a user:// folder and keep a log of which thumb ID 
-# is for which path.
