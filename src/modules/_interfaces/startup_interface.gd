828c5a06c49fc21d4178a59c69b8433d583e65cd
voylin <0voylin0@gmail.com>
2023-07-30 21:13:24 +0900

    Module Init Startup

---
+++ src/modules/_interfaces/startup_interface.gd
@@ -0,0 +1,19 @@
+class_name StartupModule extends Node
+
+## This is the interface for Startup Modules.
+##
+## The startup module is where we select which project 
+## we want to work with. A clean list of all previously
+## saved projects.
+
+
+## Get list of all projects
+
+
+## Create a new project
+
+
+## Import a new project
+
+
+## Remove project from list
