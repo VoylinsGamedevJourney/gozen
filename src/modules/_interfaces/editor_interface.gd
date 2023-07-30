c818edce4db9fd468ec162d8c5f532aa7890c27c
voylin <0voylin0@gmail.com>
2023-07-30 21:12:29 +0900

    Module Init Editor

---
+++ src/modules/_interfaces/editor_interface.gd
@@ -0,0 +1,16 @@
+class_name EditorModule extends Node
+
+## This is the interface for the Editor Module.
+##
+## The Editor is the main editing screen in which you
+## have your timeline, project view and all other tools
+## to edit your video.
+
+## Startup
+##
+## Load in the file manager module.
+## Load in the timeline module.
+## Load in the project view module.
+## Load in the effects view module.
+## Load in the command bar module.
+
