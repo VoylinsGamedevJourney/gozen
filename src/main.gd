04d2500fc58f09bb14bdea16cee8417b4695051d
voylin <0voylin0@gmail.com>
2023-08-05 09:34:19 +0900

    Borderless window

--- src/main.gd
+++ src/main.gd
@@ -1,7 +1,13 @@
-extends Control
+extends VBoxContainer
 
 
 func _ready() -> void:
-	add_child(ModuleManager.get_module("editor"))
-	add_child(ModuleManager.get_module("startup"))
-	add_child(ModuleManager.get_module("command_bar"))
+	add_child(ModuleManager.get_module("top_bar"))
+	var main_view := Control.new()
+	main_view.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	main_view.set_name("MainView")
+	add_child(main_view)
+	main_view.add_child(ModuleManager.get_module("editor"))
+	main_view.add_child(ModuleManager.get_module("startup"))
+	main_view.add_child(ModuleManager.get_module("command_bar"))
+	add_child(ModuleManager.get_module("status_bar"))
