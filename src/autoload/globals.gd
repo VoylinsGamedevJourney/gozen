950deb04db500cef288217fa9bb1c5bafb8cb787
voylin <0voylin0@gmail.com>
2023-08-09 14:56:19 +0900

    Recent projects variable

--- src/autoload/globals.gd
+++ src/autoload/globals.gd
@@ -28,5 +28,10 @@ const PATH_RECENT_PROJECTS := "user://recent_projects"
 var recent_projects: Array :
 	set(x):
 		recent_projects = x
+		# Save file
+		var file := FileAccess.open(PATH_RECENT_PROJECTS, FileAccess.WRITE)
+		file.store_var(x)
 	get:
-		return []
+		if !FileAccess.file_exists(PATH_RECENT_PROJECTS): return []
+		var file := FileAccess.open(PATH_RECENT_PROJECTS, FileAccess.READ)
+		return Array(file.get_var())
