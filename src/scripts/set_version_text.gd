9b3ba313047404f8864d4c8a4d3071b12b184b48
voylin <0voylin0@gmail.com>
2023-08-23 16:40:46 +0900

    Version check in settings now

--- src/scripts/set_version_text.gd
+++ src/scripts/set_version_text.gd
@@ -7,9 +7,9 @@ extends Label
 
 
 func _ready() -> void: 
-	set_version_string(VersionCheck.version_string)
-	VersionCheck._on_change.connect(set_version_string)
+	set_version_string(SettingsManager.get_version())
+	SettingsManager._on_version_changed.connect(set_version_string)
 
 
-func set_version_string(version_string) -> void:
-	self.text = version_string
+func set_version_string(version: String) -> void:
+	self.text = version
