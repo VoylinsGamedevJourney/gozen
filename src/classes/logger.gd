86311daf615790608fce0e3ce015e0ec1dabfabd
voylin <0voylin0@gmail.com>
2023-08-29 11:12:27 +0900

    Improved logger

--- src/classes/logger.gd
+++ src/classes/logger.gd
@@ -1,8 +1,14 @@
 class_name Logger extends Node
 
+## Disable log by adding filenames here
+const exceptions := [] 
+## Disable logs all together
+const enable_log := true
+
 
 ## Printing a line with the script name
 static func ln(message) -> void:
-	var source: String = get_stack()[1].source
-	print_rich("[b]%s:[/b] %s." % [
-			source.split('/')[-1].capitalize().replace(' ', '') ,message])
+	var file_name: String = get_stack()[1].source.split('/')[-1].capitalize().replace(' ', '')
+	if !enable_log or file_name in exceptions:
+		return
+	print_rich("[b]%s:[/b] %s." % [file_name ,message])
