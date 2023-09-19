7f291bb7c56ae71edf592712a70ab957979e12dd
voylin <0voylin0@gmail.com>
2023-09-19 17:33:26 +0900

    Some style additions

---
+++ src/assets/color_palette.gd
@@ -0,0 +1,6 @@
+class_name ColorPalette extends Resource
+
+@export var background_color := Color("#232323")
+@export var secondary_color := Color("#353535")
+@export var primary_color := Color("#595959")
+@export var accent_color := Color("#880BCB")
