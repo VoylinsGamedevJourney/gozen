91436b452a8d625dfd57dc5256bfb75e7d4834ca
voylin <0voylin0@gmail.com>
2023-11-04 08:00:45 +0900

    Small startup feature preparations

--- src/modules/default_modules/startup/startup.gd
+++ src/modules/default_modules/startup/startup.gd
@@ -6,9 +6,37 @@ extends ModuleStartup
 var recent_projects: Array
 var explorer: FileDialog
 
+var startup_images := {
+	"winter": {
+		"path": "clay-banks-u27Rrbs9Dwc-unsplash.jpg",
+		"credit_url": "https://unsplash.com/photos/u27Rrbs9Dwc",
+		"credit_name": "Clay Banks",
+		"size_y": 270
+	},
+	"spring": {
+		"path": "yusheng-deng-gNZ6MHqtsLY-unsplash.jpg",
+		"credit_url": "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/%E6%98%BC%E9%96%93%E3%81%AE%E6%B9%96%E3%82%84%E5%B1%B1%E3%81%AE%E8%BF%91%E3%81%8F%E3%81%AE%E8%91%89%E3%81%AE%E3%81%AA%E3%81%84%E6%9C%A8-gNZ6MHqtsLY",
+		"credit_name": "Yusheng Deng",
+		"size_y": 270
+	},
+	"summer": {
+		"path": "david-edelstein-N4DbvTUDikw-unsplash.jpg",
+		"credit_url": "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/%E5%AF%8C%E5%A3%AB%E5%B1%B1%E6%97%A5%E6%9C%AC-N4DbvTUDikw",
+		"credit_name": "Davide Edelstein",
+		"size_y": 270
+	},
+	"autumn": {
+		"path": "johannes-plenio-RwHv7LgeC7s-unsplash.jpg",
+		"credit_url": "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/%E5%A4%AA%E9%99%BD%E5%85%89%E7%B7%9A%E3%81%AB%E3%82%88%E3%82%8B%E6%A3%AE%E6%9E%97%E7%86%B1-RwHv7LgeC7s",
+		"credit_name": "Johannes Plenio",
+		"size_y": 270
+	},
+}
+const CREDIT_TEXT := "[right][i]Image by [url=%s]%s via Unsplash[/url][/i][/right]"
+const IMAGE_PATH := "res://assets/images_startup/%s"
+
 
 func _ready() -> void:
-	
 	ProjectManager.get_recent_projects()
 	
 	# Check if opened with a "*.gozen" file as argument.
@@ -34,6 +62,23 @@ func _ready() -> void:
 		new_button.pressed.connect(_on_recent_project_button_pressed.bind(path))
 		new_button.visible = true
 		%RecentProjectsVBox.add_child(new_button)
+	
+	# Setting the startup image:
+	var month : int = Time.get_datetime_dict_from_system().month
+	var image_data: Dictionary = startup_images["winter"]
+	# TODO: Find better images for this
+	#match month:
+		#12,1,2:
+			#image_data = startup_images["winter"]
+		#3,4,5:
+			#image_data = startup_images["spring"]
+		#6,7,8:
+			#image_data = startup_images["summer"]
+		#9,10,11:
+			#image_data = startup_images["spring"]
+	%ImageCredit.text = CREDIT_TEXT % [image_data.credit_url, image_data.credit_name]
+	%WelcomeImage.texture = load(IMAGE_PATH % image_data.path)
+	%WelcomeImage.custom_minimum_size.y = image_data.size_y
 
 
 func _on_url_clicked(meta) -> void:
