5a6298e4150884911262dc83b958000c94b13d05
voylin <0voylin0@gmail.com>
2023-11-04 14:05:09 +0900

    Thumbnails with ffmpeg

--- gd_extension/src/gozen_interface.cpp
+++ gd_extension/src/gozen_interface.cpp
@@ -9,8 +9,20 @@
 
 
 void GoZenInterface::get_thumb(String file_path, String dest_path) {
-  UtilityFunctions::print("Path of file is:");
-  UtilityFunctions::print(file_path);
+  UtilityFunctions::print_rich("[b]GoZenInterface:[/b] Generating thumbnail for " + file_path);
+
+  // FFmpeg command to extract the first frame
+  std::string path_file = file_path.utf8().get_data();
+  std::string path_dest = dest_path.utf8().get_data();
+  std::string ffmpegCommand = "ffmpeg -i " + path_file + " -vf \"select=eq(n\\,0)\" -vframes 1 " + path_dest;
+
+  int status = std::system(ffmpegCommand.c_str());
+
+  if (status != 0) {
+    UtilityFunctions::print_rich("[b]GoZenInterface:[/b] Thumb created successfully!");
+  } else {
+    UtilityFunctions::printerr("Error extracting the first frame.");
+  }
 }
 
 void GoZenInterface::_bind_methods() {
--- src/test_room/test_room.gd
+++ src/test_room/test_room.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _ready() -> void:
 	var interface: GoZenInterface = GoZenInterface.new()
-	interface.get_thumb("test", "test2")
+	interface.get_thumb("/home/voylin/Documents/Programming/GoZen/src/test_room/test.mp4", "/home/voylin/Documents/Programming/GoZen/src/test_room/test.jpg")
