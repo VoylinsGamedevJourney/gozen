2adf788d43963364d925c0d9ff5fe23ee8c63136
Manpreet Singh <63737630+ManpreetSingh2004@users.noreply.github.com>
2024-02-08 19:24:54 +0530

    Fix window maximize on windows
    Added a temporary fix for maximizing the window.

--- src/ui/screen_main/scripts/main_top_bar.gd
+++ src/ui/screen_main/scripts/main_top_bar.gd
@@ -1,5 +1,64 @@
 extends PanelContainer
 
+###############################################################
+#region Window Mode  ##########################################
+###############################################################
+
+@export var resize_handles: Control
+
+var _windowed_win_size: Vector2i = Vector2i(300, 100)
+var _windowed_win_pos: Vector2i = Vector2i(0, 0)
+var _win_mode: Window.Mode = Window.MODE_WINDOWED
+
+var win_mode: Window.Mode:
+	get:
+		return _win_mode
+
+	set(value):
+		if value == _win_mode: return
+
+		# store prev_mode and current win_mode
+		var prev_mode = _win_mode
+		_win_mode = value
+
+		if OS.get_name() != "Windows" or !get_window().borderless:
+			get_window().mode = value
+			return
+
+		# store window size in windowed mode
+		if prev_mode == Window.MODE_WINDOWED:
+			_windowed_win_pos = get_window().position
+			_windowed_win_size = get_window().size
+
+		if value == Window.MODE_MAXIMIZED:
+			get_window().borderless = false
+			get_window().mode = Window.MODE_MAXIMIZED
+			get_window().borderless = true
+			
+			# adjust mismatched window size and position
+			var usable_screen := DisplayServer.screen_get_usable_rect(get_window().current_screen)
+			get_window().position = usable_screen.position + Vector2i(1, 1)
+			get_window().size = usable_screen.size
+			return
+
+		if value == Window.MODE_WINDOWED:
+			get_window().borderless = false
+			get_window().mode = prev_mode	# window.mode is not set to maximized when borderless
+			get_window().mode = Window.MODE_WINDOWED
+			get_window().borderless = true
+			
+			# restore window size and position
+			get_window().size = _windowed_win_size
+			get_window().position = _windowed_win_pos
+			return
+
+		get_window().mode = value
+
+#endregion
+###############################################################
+#region Main  #################################################
+###############################################################
+
 var move_window := false
 var move_start: Vector2i
 
@@ -9,16 +68,21 @@ func _ready() -> void:
 	ProjectManager._on_project_loaded.connect(func(): 
 		$Margin/HBox/ProjectButton.visible = true)
 
+	assert(resize_handles, "Resize handles not assigned.")
+	get_viewport().size_changed.connect(func():
+		resize_handles.visible = win_mode == Window.MODE_WINDOWED and get_window().borderless
+	)
 
+#endregion
 ###############################################################
 #region Window Dragging  ######################################
 ###############################################################
 
 func _on_top_bar_dragging(event):
 	if event is InputEventMouseButton and event.button_index == 1:
-		if !move_window:
+		move_window = event.is_pressed() and win_mode == Window.MODE_WINDOWED
+		if move_window:
 			move_start = get_viewport().get_mouse_position()
-		move_window = event.is_pressed()
 
 
 func _process(_delta: float) -> void:
@@ -32,14 +96,14 @@ func _process(_delta: float) -> void:
 ###############################################################
 
 func _on_minimize_button_pressed():
-	get_window().set_mode(Window.MODE_MINIMIZED)
+	win_mode = Window.MODE_MINIMIZED
 
 
 func _on_switch_mode_button_pressed():
-	if get_window().mode == Window.MODE_WINDOWED:
-		get_window().set_mode(Window.MODE_FULLSCREEN)
-	elif get_window().mode == Window.MODE_FULLSCREEN:
-		get_window().set_mode(Window.MODE_WINDOWED)
+	if win_mode == Window.MODE_WINDOWED:
+		win_mode = Window.MODE_MAXIMIZED
+	else:
+		win_mode = Window.MODE_WINDOWED
 
 
 func _on_exit_button_pressed():
