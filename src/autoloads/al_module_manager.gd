0c935e7bacd377702da4f6ebf0929d284c3a6fb1
voylin <0voylin0@gmail.com>
2024-03-08 15:23:55 +0900

    Improved module manager

--- src/autoloads/al_module_manager.gd
+++ src/autoloads/al_module_manager.gd
@@ -1,35 +1,63 @@
 extends Node
+## Module manager
+##
+## This autoload is responsible for loading in and checking all custom modules
+## to make certain they were made correctly. 
+
+const types := ["layout_modules", "modules", "effects", "transitions", "render_profiles"]
 
 
 func _ready():
-	var dir := DirAccess.open("user://")
-	var settings_path := "globals/path/modules/%s"
-	var types := ["layout_modules", "modules", "effects", "transitions", "render_profiles"]
-	
-	# Load all custom modules
+	## Creating necessary structures and loading/checking all custom modules
+	_create_folder_structure()
+	_load_custom_modules()
+	_check_custom_modules()
+
+
+func _create_folder_structure() -> void:
+	## Function for creating the folders for saving module configs to and
+	## for creating the folders to save custom modules in to load on startup.
+	var existing_folders := DirAccess.get_directories_at("user://")
+	for folder_type: String in types:
+		var paths: PackedStringArray = [
+			ProjectSettings.get_setting("globals/path/configs/%s" % folder_type),
+			ProjectSettings.get_setting("globals/path/modules/%s" % folder_type)] 
+		for path: String in paths:
+			if not path in existing_folders:
+				DirAccess.make_dir_absolute(path)
+
+
+func _load_custom_modules() -> void:
 	for type: String in types:
-		var _path: String = ProjectSettings.get_setting(settings_path % type)
-		if !dir.dir_exists(_path):
-			dir.make_dir(_path)
-			continue
-		dir = dir.open(_path)
-		for pck_file: String in dir.get_files():
-			ProjectSettings.load_resource_pack(pck_file, false)
-	
-	# Check if all modules have an info resource
+		var module_folder: String = ProjectSettings.get_setting("globals/path/modules/%s" % type)
+		var module_files := DirAccess.get_files_at(module_folder)
+		if module_files.size() == 0:
+			continue # Skipping when no modules are present
+		for module: String in module_files:
+			ProjectSettings.load_resource_pack(module_folder + module, false)
+
+
+func _check_custom_modules() -> void:
+	## Check if all modules have an info resource
 	for type: String in types:
-		dir = dir.open("res://%s" % type)
-		var mod_dirs := dir.get_directories()
-		var mod_res := dir.get_files()
+		var module_folders := DirAccess.get_directories_at("res://%s" % type)
+		var module_resources := DirAccess.get_files_at("res://%s" % type)
 		
 		# Checking for mod configs
-		for mod: String in mod_res:
-			if not mod.trim_suffix(".tres") in mod_dirs:
-				Printer.error("No folder for config '%s'!" % mod)
-				get_tree().quit() # TODO: Handle this better
+		for module: String in module_resources:
+			if not module.trim_suffix(".tres") in module_folders:
+				Printer.error("No folder for config '%s'!" % module.trim_suffix(".tres"))
+				get_tree().quit() # TODO: Handle this in a better ways
 		
 		# Checking for mod folders
-		for mod: String in mod_dirs:
-			if not mod + ".tres" in mod_res:
-				Printer.error("No config for mod folder '%s'!" % mod)
-				get_tree().quit() # TODO: Handle this better
+		for module: String in module_folders:
+			if not module + ".tres" in module_resources:
+				Printer.error("No config for mod folder '%s'!" % module)
+				get_tree().quit() # TODO: Handle this in a better way
+
+
+func get_config_path(type: String, instance_name: String) -> String:
+	return "%s%s.cfg" % [
+		ProjectSettings.get_setting("globals/path/configs/%s" % type), 
+		instance_name
+	]
