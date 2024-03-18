225b6b7c06488cc824bc9efe754b29973338e804
voylin <0voylin0@gmail.com>
2024-03-18 17:23:14 +0900

    Render menu layout init

---
+++ src/_layout_modules/layout_render_menu/layout_render_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+# TODO: Mini timeline should just show beginning and ending and will only be 
+# used for quickly checking the video and to eventually select an area of the 
+# video which has to be rendered separately.
+# TODO: ProjectView will be to display where the marker is on the mini timeline
+# This can not be the project_view module as it's possible that people have
+# their own project view module which they'd like to use.
+# TODO: Create a render profile system in form of a resource/Dictionary which
+# can be saved to a pck file or preferably just a text file instead so we can
+# use a RenderProfile class and use var_to_str and str_to_var
+# TODO: Render settings should have a dropdown with the necesarry settings
+# displayed underneath the dropdown which change after selecting a render profile.
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
