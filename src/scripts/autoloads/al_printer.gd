c3285b41443a7253c39d486694740c34c5548a34
voylin <0voylin0@gmail.com>
2024-03-19 15:23:17 +0900

    Basic log file system

--- src/scripts/autoloads/al_printer.gd
+++ src/scripts/autoloads/al_printer.gd
@@ -5,7 +5,12 @@ extends Node
 #          when debug build. For Release there will be a toggle in settings
 #          to enable logging.
 
+var log_file : FileAccess
+var log_path : String
+
+
 func _ready() -> void:
+	_create_log()
 	_print_wall("purple", [
 		["--==  GoZen - Video Editor  ==--"],
 		["GoZen version", ProjectSettings.get_setting("application/config/version")],
@@ -34,17 +39,40 @@ func error(message: String) -> void:
 
 
 func debug(message: String) -> void:
-	todo("Have a toggle in settings to enable/disable debug.")
-	_print(["grey", "DEBUG", message])
+	if SettingsManager.get_debug_enabled():
+		_print(["grey", "DEBUG", message])
 
 
 func _print(data: PackedStringArray) -> void:
+	_add_to_log("%s: %s" % [data[1], data[2]])
 	print_rich("[color=%s][b]%s:[/b] %s" % [data[0], data[1], data[2]])
 
 
 func _print_wall(color: String, data: Array) -> void:
 	for line: PackedStringArray in data:
 		if line.size() == 1:
+			_add_to_log(line[0])
 			print_rich("[color=%s][b]%s[/b]" % [color, line[0]])
 			continue
+		_add_to_log("%s: %s" % [line[0], line[1]])
 		print_rich("[color=%s][b]%s:[/b] %s" % [color, line[0], line[1]])
+
+
+###############################################################
+#region Log File Handler  #####################################
+###############################################################
+
+func _create_log() -> void:
+	var date_time: Dictionary = Time.get_datetime_dict_from_system()
+	log_path = "user://log_{year}-{month}-{day}_{hour}-{minute}-{second}".format(date_time)
+	log_file = FileAccess.open(log_path, FileAccess.WRITE)
+
+
+func _add_to_log(message: String) -> void:
+	if !log_file.is_open():
+		print("not open")
+		log_file = FileAccess.open(log_path, FileAccess.WRITE)
+	log_file.store_line(message)
+
+#endregion
+###############################################################
