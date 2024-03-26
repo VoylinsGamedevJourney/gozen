3234f80e8875eee79eccfa7cfda312dd1cff0c50
voylin <0voylin0@gmail.com>
2024-03-26 16:15:45 +0900

    Gengodot init

---
+++ src/addons/gengodot/gengodot_script.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+const POT_LOCATION := "res://translations/gozen-translations/po_files/translations_template.pot"
+const PO_LOCATION := "res://translations/gozen-translations/po_files/"
+
+#const POT_HEADERS := [
+	#""
+#] ""Project-Id-Version: GoZen - Video Editor\n"
+#"MIME-Version: 1.0\n"
+#"Content-Type: text/plain; charset=UTF-8\n"
+#"Content-Transfer-Encoding: 8-bit\n""
+
+
+var ids := []
+var first_language := []
+var second_language := []
+
+
+func generate_pot() -> void:
+	
+	pass
