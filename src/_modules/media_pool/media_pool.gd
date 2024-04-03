36cdae28380a8413e86832dcadf560af60fc172c
voylin <0voylin0@gmail.com>
2024-04-03 14:11:00 +0900

    Media pool update

--- src/_modules/media_pool/media_pool.gd
+++ src/_modules/media_pool/media_pool.gd
@@ -1,32 +1,16 @@
 extends PanelContainer
+# TODO: Handle global file data separately, maybe have a global media pool or something
+# or find a better way of handling data11
 
-enum STATE { PROJECT, GLOBAL }
 
-var state: STATE
 var icon_folder := preload("res://assets/icons/folder.png")
 
 
-func _ready() -> void:
-	# Starting with project files
-	_on_switch_project_button_pressed()
+func _on_project_tree_button_pressed() -> void:
+	%ProjectScroll.visible = true
+	%GlobalScroll.visible = false
 
 
-#region #####################  Top area  #######################################
-
-func _on_switch_project_button_pressed() -> void:
-	$VBox/ProjectPanelScroll.visible = true
-	$VBox/GlobalPanelScroll.visible = false
-	state = STATE.PROJECT
-
-
-func _on_switch_global_button_pressed() -> void:
-	$VBox/ProjectPanelScroll.visible = false
-	$VBox/GlobalPanelScroll.visible = true
-	state = STATE.GLOBAL
-
-
-func _on_search_line_edit_text_changed(_new_text: String) -> void:
-	pass # TODO: Make this work (Just hide all files which don't include the search
-	# word in the file name. All folders with all files hidden should also get hidden)
-
-#endregion
+func _on_global_tree_button_pressed() -> void:
+	%ProjectScroll.visible = false
+	%GlobalScroll.visible = true
--- src/_modules/media_pool/tree.gd
+++ src/_modules/media_pool/tree.gd
@@ -1,22 +1,27 @@
 extends Tree
+# TODO: Remope folder/file in submenu
 
 enum POPUP_TYPE { MINIMAL, FULL }
 
+
 @export var global: bool = false
 
-var icon_folder := preload("res://assets/icons/folder.png")
-var icon_max_width := 20
+var icon_folder: Texture2D = preload("res://assets/icons/folder.png")
+var icon_max_width: int = 20
 
-var folder_items := {}
-var file_items := {}
+var folder_items: Dictionary = {}
+var file_items: Dictionary = {}
 
 
 func _ready() -> void:
 	# Creating the root item
-	var root := create_item()
-	root.set_text(0, "root")
-	root.set_icon(0, icon_folder)
-	root.set_icon_max_width(0, icon_max_width)
+	var l_root: TreeItem = create_item()
+	l_root.set_text(0, "root")
+	l_root.set_icon(0, icon_folder)
+	l_root.set_icon_max_width(0, icon_max_width)
+	folder_items[l_root] = '/'
+	
+	get_window().files_dropped.connect(files_dropped)
 	
 	# Loading structure
 	if global:
@@ -29,80 +34,88 @@ func get_folder_data() -> Dictionary:
 	return FileManager.folder_data if global else ProjectManager.folder_data
 
 
-func get_file_data() -> Dictionary:
-	return FileManager.folder_data if global else ProjectManager.folder_data
-
-
 func load_structure() -> void:
 	# Loading in all folders and folders
-	var folder_data := get_folder_data()
-	for path: String in folder_data:
-		_load_folders(path, folder_data[path])
+	for l_path: String in get_folder_data():
+		_load_folders(l_path, get_folder_data()[l_path])
 
 
-func _load_folders(path: String, files: Array) -> void:
+func _load_folders(a_path: String, a_files: Array) -> void:
+	if a_path == "/":
+		_load_files(get_root(), a_files)
+		return
+	
 	# Creating the folder structure
-	var current_folder: TreeItem = get_root()
-	for folder: String in path.split('/'):
-		current_folder = _new_folder(current_folder, folder, path)
-	_load_files(current_folder, files)
+	var l_current_folder: TreeItem = get_root()
+	
+	for l_folder: String in a_path.split('/'):
+		l_current_folder = _new_folder(l_current_folder, l_folder, a_path)
+	
+	_load_files(l_current_folder, a_files)
 
 
-func _load_files(folder: TreeItem, files: PackedStringArray) -> void:
+func _load_files(a_folder: TreeItem, a_files: PackedStringArray) -> void:
 	# Adding all files to the folder structure
-	for file_id: String in files:
-		var file_data: File = get_file_data()[file_id]
-		_new_file(folder, file_id, file_data)
+	for l_file_id: String in a_files:
+		var l_file_data: File = FileManager.get_files_data(global)[l_file_id]
+		_new_file(a_folder, l_file_id, l_file_data)
 
 
-func _new_folder(parent: TreeItem, folder_name: String, folder_path: String) -> TreeItem:
-	var item := create_item(parent)
-	item.set_text(0, folder_name)
-	item.set_icon(0, icon_folder)
-	item.set_icon_max_width(0, icon_max_width)
-	folder_items[item] = folder_path
-	return item
+func _new_folder(a_parent: TreeItem, a_folder_name: String, a_folder_path: String) -> TreeItem:
+	var l_item := create_item(a_parent)
+	
+	l_item.set_text(0, a_folder_name)
+	l_item.set_icon(0, icon_folder)
+	l_item.set_icon_max_width(0, icon_max_width)
+	
+	folder_items[l_item] = a_folder_path
+	return l_item
 
 
 func _new_file(parent: TreeItem, file_id: String, data: File) -> TreeItem:
 	var item := create_item(parent)
+	
 	item.set_text(0, data.nickname)
 	item.set_icon(0, data.get_icon())
 	item.set_icon_max_width(0, icon_max_width)
+	
 	if data is FileActual:
 		item.set_tooltip_text(0, data.file_path)
+	
 	file_items[item] = file_id
 	return item
 
 
-func create_popup(type: POPUP_TYPE, item: TreeItem) -> PopupMenu:
+func create_popup(a_type: POPUP_TYPE, a_item: TreeItem) -> PopupMenu:
 	# TODO: Add to popup manager
-	var popup := PopupMenu.new()
-	popup.id_pressed.connect(_on_item_clicked.bind(popup, item))
-	popup.mouse_exited.connect(func(): popup.queue_free())
-	popup.position = get_global_mouse_position() as Vector2i
-	popup.size = Vector2i(160, 10)
-	popup.add_item("Add file(s)", 0)
-	popup.add_item("Add folder", 1)
-	if type == POPUP_TYPE.MINIMAL:
-		return popup
-	popup.add_separator()
-	popup.add_item("Rename", 2)
-	return popup
+	var l_popup := PopupMenu.new()
+	
+	l_popup.id_pressed.connect(_on_item_clicked.bind(l_popup, a_item))
+	l_popup.mouse_exited.connect(Toolbox.free_node.bind(l_popup))
+	l_popup.position = get_global_mouse_position() as Vector2i
+	l_popup.size = Vector2i(160, 10)
+	
+	l_popup.add_item("Add file(s)", 0)
+	l_popup.add_item("Add folder", 1)
+	
+	if a_type != POPUP_TYPE.MINIMAL:
+		l_popup.add_separator()
+		l_popup.add_item("Rename", 2)
+	
+	return l_popup
 
 
-func _on_empty_clicked(_mouse_position: Vector2, mouse_button_index: int) -> void:
-	if mouse_button_index == MOUSE_BUTTON_RIGHT:
-		var popup := create_popup(POPUP_TYPE.MINIMAL, get_root())
-		add_child(popup)
-		popup.popup()
+func _on_empty_clicked(_mouse_position: Vector2, a_mouse_button_index: int) -> void:
+	if a_mouse_button_index == MOUSE_BUTTON_RIGHT:
+		var l_popup: PopupMenu = create_popup(POPUP_TYPE.MINIMAL, get_root())
+		add_child(l_popup)
+		l_popup.popup()
 
 
 func _on_item_mouse_selected(_position: Vector2, mouse_button_index: int) -> void:
 	# TODO: Double click = rename
 	if mouse_button_index == MOUSE_BUTTON_RIGHT:
-		var item: TreeItem = get_selected()
-		var popup := create_popup(POPUP_TYPE.FULL, item)
+		var popup := create_popup(POPUP_TYPE.FULL, get_selected())
 		add_child(popup)
 		popup.popup()
 
@@ -118,49 +131,63 @@ func _on_item_clicked(id: int, popup: PopupMenu, item: TreeItem) -> void:
 	popup.queue_free()
 
 
-func open_file_dialog(folder: TreeItem) -> void:
-	var dialog := DialogManager.get_file_import_dialog()
-	dialog.files_selected.connect(add_files.bind(folder))
-	dialog.file_selected.connect(add_file.bind(folder))
-	dialog.canceled.connect(func() -> void: dialog.queue_free())
-	add_child(dialog)
-	dialog.popup_centered(Vector2i(500,600))
+func open_file_dialog(a_folder: TreeItem) -> void:
+	var l_dialog: FileDialog = DialogManager.get_file_import_dialog()
+	
+	l_dialog.files_selected.connect(add_files.bind(a_folder))
+	l_dialog.file_selected.connect(add_file.bind(a_folder))
+	l_dialog.canceled.connect(Toolbox.free_node.bind(l_dialog))
+	
+	add_child(l_dialog)
+	l_dialog.popup_centered(Vector2i(500,600))
 
 
-func add_folder(folder: TreeItem) -> void:
+func add_folder(a_folder: TreeItem) -> void:
 	# Creating path
-	var path := "new_folder"
-	var current_folder := folder
+	var l_path: String = "new_folder"
+	var l_current_folder: TreeItem = a_folder
+	
 	while true:
-		path = "%s/%s" % [current_folder.get_text(0), path]
-		current_folder = current_folder.get_parent()
-		if !current_folder:
-			if path.split('/')[0] == "root":
-				path = path.trim_prefix("root/")
+		l_path = "%s/%s" % [l_current_folder.get_text(0), l_path]
+		l_current_folder = l_current_folder.get_parent()
+		if !l_current_folder:
+			if l_path.split('/')[0] == "root":
+				l_path = l_path.trim_prefix("root/")
 			break
 	
 	# Checking if folder name is taken already or not
-	var keys: PackedStringArray = get_folder_data().keys()
-	while path in keys:
-		if int(path[-1]) in range(10):
-			var nr: int = path.split("_")[-1].to_int()
-			path = "%s_%s" % [path.trim_suffix('_' + str(nr)), str(nr + 1)]
+	while l_path in get_folder_data().keys():
+		if int(l_path[-1]) in range(10):
+			var l_nr: int = l_path.split("_")[-1].to_int()
+			l_path = "%s_%s" % [l_path.trim_suffix('_' + str(l_nr)), str(l_nr + 1)]
 		else:
-			path += "_1"
+			l_path += "_1"
 	
-	_new_folder(folder, path.split('/')[-1], path)
-	FileManager.add_folder(path, FileManager.folder_data if global else ProjectManager.folder_data)
+	_new_folder(a_folder, l_path.split('/')[-1], l_path)
+	FileManager.add_folder(l_path, global)
 	# TODO: Directly focus on renaming and only saving new folder after by calling a rename_item function?
 
 
-func add_file(path: String, folder: TreeItem) -> void:
-	print("%s - %s" % [path, folder])
+func add_file(a_file_path: String, a_folder_item: TreeItem) -> void:
+	var l_file_id: String = FileManager.add_file_actual(a_file_path, folder_items[a_folder_item], global)
+	var l_file_obj: FileActual = FileManager.get_file_obj(l_file_id, global)
+	var l_file_item := a_folder_item.create_child()
+	
+	file_items[l_file_item] = l_file_id
+	l_file_item.set_text(0, l_file_obj.nickname)
+	l_file_item.set_icon(0, l_file_obj.get_icon())
+	l_file_item.set_icon_max_width(0, icon_max_width)
 
 
-func add_files(paths: PackedStringArray, folder: TreeItem) -> void:
-	for path: String in paths:
-		add_file(path, folder)
+func add_files(a_file_paths: PackedStringArray, a_folder_item: TreeItem) -> void:
+	for l_file_path: String in a_file_paths:
+		add_file(l_file_path, a_folder_item)
 
 
-func rename_item(_item: TreeItem) -> void:
-	pass # TODO: Make this work
+func rename_item(a_item: TreeItem) -> void:
+	print("not working") # TODO: Make this work
+
+
+func files_dropped(a_files: PackedStringArray) -> void:
+	if is_visible_in_tree():
+		add_files(a_files, get_item_at_position(get_local_mouse_position()))
