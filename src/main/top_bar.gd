939269309c2d4f514cbfea2bb71660546f5d3294
voylin <0voylin0@gmail.com>
2024-04-06 08:54:12 +0900

    Main UI refactoring

--- src/main/top_bar.gd
+++ src/main/top_bar.gd
@@ -1,5 +1,22 @@
 extends HBoxContainer
 
+@onready var label_window_title: Label = $TopBarPanel/WindowTitleLabel
+
+
+func _ready() -> void:
+	label_window_title.text = tr("TEXT_UNTITLED_PROJECT_TITLE")
+	ProjectManager._on_title_changed.connect(_on_project_title_changed)
+	ProjectManager._on_unsaved_changes_changed.connect(_on_project_unsaved_changes_changed)
+
+
+func _on_project_title_changed(a_title: String) -> void:
+	# Extra space is needed for the '*' mark to indicate unsaved changes
+	label_window_title.text = a_title + " "
+
+
+func _on_project_unsaved_changes_changed(a_value: bool) -> void:
+	label_window_title.text[-1] = "*" if a_value else " "
+
 
 func _on_settings_button_pressed() -> void:
 	PopupManager.open_popup(PopupManager.POPUP.SETTINGS_MENU)
