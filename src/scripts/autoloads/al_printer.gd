6ad8330967a824feda8b4797eb7f57126a7b127b
voylin <0voylin0@gmail.com>
2024-04-22 11:22:49 +0900

    Printer update

--- src/scripts/autoloads/al_printer.gd
+++ src/scripts/autoloads/al_printer.gd
@@ -23,8 +23,8 @@ func _ready() -> void:
 		["Processor", OS.get_processor_name()],
 		["Threads", OS.get_processor_count()],
 		["Ram", "\n\tTotal: %s GB\n\tAvailable: %s GB" % [
-			str("%0.2f" % (float(OS.get_memory_info().physical)/1_073_741_824)), 
-			str("%0.2f" % (float(OS.get_memory_info().available)/1_073_741_824))]],
+			str("%0.2f" % (OS.get_memory_info().physical/1_073_741_824)), 
+			str("%0.2f" % (OS.get_memory_info().available/1_073_741_824))]],
 		["Video adapter", "\n\tName: %s\n\tVersion: %s\n\tType: %s" % [
 			RenderingServer.get_video_adapter_name(),
 			RenderingServer.get_video_adapter_api_version(),
@@ -34,37 +34,50 @@ func _ready() -> void:
 	])
 
 
-func todo(message: String) -> void:
-	_print(["green", "TODO", message])
+func todo(a_message: String) -> void:
+	_print(["green", "TODO", a_message])
 
 
-func error(message: String) -> void:
-	_print(["red", "ERROR", message])
+func connect_error(a_error: int) -> void:
+	if !a_error:
+		return
+	var l_message: String = "Connect error '%s'" % a_error
+	l_message += "\n\tFile: '%s'" % get_stack()[1].source
+	l_message += "\n\tFunction: '%s'" % get_stack()[1].function
+	l_message += "\n\tLine: '%s'" % get_stack()[1].line
+	_print(["red", "ERROR", l_message])
 
 
-func debug(message: String) -> void:
+func error(a_message: String) -> void:
+	a_message += "\n\tFile: '%s'" % get_stack()[1].source
+	a_message += "\n\tFunction: '%s'" % get_stack()[1].function
+	a_message += "\n\tLine: '%s'" % get_stack()[1].line
+	_print(["red", "ERROR", a_message])
+
+
+func debug(a_message: String) -> void:
 	if SettingsManager.get_debug_enabled():
-		_print(["grey", "DEBUG", message])
+		_print(["grey", "DEBUG", a_message])
 
 
-func _print(data: PackedStringArray) -> void:
-	_add_to_log("%s: %s" % [data[1], data[2]])
-	print_rich("[color=%s][b]%s:[/b] %s" % [data[0], data[1], data[2]])
+func _print(a_data: PackedStringArray) -> void:
+	_add_to_log("%s: %s" % [a_data[1], a_data[2]])
+	print_rich("[color=%s][b]%s:[/b] %s" % [a_data[0], a_data[1], a_data[2]])
 
 
-func _print_wall(color: String, data: Array) -> void:
-	for line: PackedStringArray in data:
-		if line.size() == 1:
-			_add_to_log(line[0])
-			print_rich("[color=%s][b]%s[/b]" % [color, line[0]])
+func _print_wall(a_color: String, a_data: Array) -> void:
+	for l_line: PackedStringArray in a_data:
+		if l_line.size() == 1:
+			_add_to_log(l_line[0])
+			print_rich("[color=%s][b]%s[/b]" % [a_color, l_line[0]])
 			continue
-		_add_to_log("%s: %s" % [line[0], line[1]])
-		print_rich("[color=%s][b]%s:[/b] %s" % [color, line[0], line[1]])
+		_add_to_log("%s: %s" % [l_line[0], l_line[1]])
+		print_rich("[color=%s][b]%s:[/b] %s" % [a_color, l_line[0], l_line[1]])
 
 
-func _add_to_log(message: String) -> void:
+func _add_to_log(a_message: String) -> void:
 	if !Engine.has_singleton("EditorInterface"):
-		message = "{hour}-{minute}-{second}:  " + message
+		a_message = "{hour}-{minute}-{second}:  " + a_message
 		log_file.seek_end()
-		log_file.store_line(message.format(Time.get_time_dict_from_system()))
+		log_file.store_line(a_message.format(Time.get_time_dict_from_system()))
 		log_file.flush()
