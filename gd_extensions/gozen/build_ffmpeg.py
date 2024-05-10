3b2224bfd6ffb6be66b9258bec1adb2f0ffe8a40
voylin <0voylin0@gmail.com>
2024-05-10 10:41:32 +0900

    Pyton update
    Styling update to spaces

--- gd_extensions/gozen/build_ffmpeg.py
+++ gd_extensions/gozen/build_ffmpeg.py
@@ -1,17 +1,16 @@
+import toolbox
 import sys
 import os
-import shutil
 import platform
-import glob
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../python"))
-import toolbox
-
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "../../python"))
 
 
-folder_bin = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../bin/ffmpeg_bin')
-folder_source = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ffmpeg')
-
+folder_bin = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '../../bin/ffmpeg_bin')
+folder_source = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), 'ffmpeg')
 
 
 def configure_ffmpeg():
@@ -19,9 +18,11 @@ def configure_ffmpeg():
     if platform.system().lower() == 'linux':
         os.environ['PATH'] = '/opt/bin:' + os.environ['PATH']
         cross_prefix = 'x86_64-w64-mingw32-'
-        config_extra_args = '--cross-prefix=' + cross_prefix + ' --arch=x86_64 --target-os=mingw32'
-    
-    os.system(f'{folder_source}/configure --prefix={folder_bin} --enable-gpl --enable-shared {config_extra_args}')
+        config_extra_args = '--cross-prefix=' + \
+            cross_prefix + ' --arch=x86_64 --target-os=mingw32'
+
+    os.system(f'{folder_source}/configure --prefix={
+              folder_bin} --enable-gpl --enable-shared {config_extra_args}')
 
 
 def make_ffmpeg(a_num_jobs):
@@ -30,13 +31,12 @@ def make_ffmpeg(a_num_jobs):
     os.system(f'make -j {a_num_jobs} install')
 
 
-def build(a_num_jobs = 0):
-    if a_num_jobs == 0: # Getting threads/cores for compiling FFmpeg
+def build(a_num_jobs=0):
+    if a_num_jobs == 0:  # Getting threads/cores for compiling FFmpeg
         a_num_jobs = toolbox.get_input_jobs()
 
-    configure_ffmpeg() # Configuring FFmpeg build
-    make_ffmpeg(a_num_jobs) # Building ffmpeg
-        
+    configure_ffmpeg()  # Configuring FFmpeg build
+    make_ffmpeg(a_num_jobs)  # Building ffmpeg
 
 
 if __name__ == '__main__':
--- translations/translations.py
+++ translations/translations.py
@@ -1,15 +1,17 @@
+import toolbox
 import sys
 import os
 import re
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../python'))
-import toolbox
-
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '../python'))
 
 
 def generate_pot():
-    # Go through files in 'src' (Exclude .godot and only look in *.tscn and *.gd files)
-    # Make a dictionary of all entries, which line they appear on, msgctx from parameters
+    # Go through files in 'src'
+    # (Exclude .godot and only look in *.tscn and *.gd files)
+    # Make a dictionary of all entries, which line they appear on,
+    # msgctx from parameters
     # Generate file from dictionary
     results = {}
     files_list = []
@@ -20,12 +22,15 @@ def generate_pot():
                 _scan_tscn_file(os.path.join(root, file), results, files_list)
 
     # For testing only
-    #for key, value in results.items():
+    # for key, value in results.items():
     #    print(key)
     #    #for entry in value:
-    #    #    print(f'    File: {entry['file_path']}, Block: {entry['block_number']}, Line: {entry['line_number']}')
+    #    #    print(f'    File: {entry['file_path']},
+        # #	Block: {entry['block_number']}, Line: {entry['line_number']}')
     with open('translations_template.pot', 'w') as file:
-        file.write('# LANGUAGE translation for GoZen - Video Editor for the following files:\n')
+        file.write(
+            '# LANGUAGE translation for GoZen -\
+            Video Editor for the following files:\n')
         for file_path in sorted(files_list):
             file.write(f'# {file_path.replace("../src", "res:/")}\n')
         file.write('#, fuzzy\nmsgid ""\nmsgstr ""\n')
@@ -35,20 +40,17 @@ def generate_pot():
         file.write('"Content-Transfer-Encoding: 8-bit\\n"\n')
         for key, value in results.items():
             file.write('\n')
-            file.write(f'#: {value["file_path"].replace("../src", "res:/")}:{value["line_number"]}\n')
-            file.write(f'#: node={value["node_type"]}, type = {value["text_type"]}\n')
+            file.write(f'#: {value["file_path"].replace(
+                "../src", "res:/")}:{value["line_number"]}\n')
+            file.write(f'#: node={value["node_type"]}, type = {
+                       value["text_type"]}\n')
             file.write(f'msgid "{key}"\nmsgstr ""\n')
-
-
-
-
-
 
 
 def _scan_tscn_file(a_file_path, a_results, a_files_list):
     with open(a_file_path, 'r') as f:
         lines = f.readlines()
-    
+
     node_type = ''
     text = ''
     text_line_number = ''
@@ -60,7 +62,7 @@ def _scan_tscn_file(a_file_path, a_results, a_files_list):
             temp_node_type = re.search(r'type="([^"]+)"', line)
             if temp_node_type:
                 node_type = temp_node_type.group(1)
-        
+
         if line.startswith("text = "):
             text = re.search(r'text = "([^"]+)"', line).group(1)
             text_line_number = line_number
@@ -68,7 +70,8 @@ def _scan_tscn_file(a_file_path, a_results, a_files_list):
             text = re.search(r'tooltip_text = "([^"]+)"', line).group(1)
             tooltip_text_line_number = line_number
         elif line.startswith("auto_translate = "):
-            auto_translate = re.search(r'auto_translate = ([^"]+)', line).group(1) != 'false'
+            auto_translate = re.search(
+                r'auto_translate = ([^"]+)', line).group(1) != 'false'
 
         if line == '\n':
             # Block ended
@@ -77,14 +80,14 @@ def _scan_tscn_file(a_file_path, a_results, a_files_list):
                     a_files_list.append(a_file_path)
                 if text != '':
                     a_results[text] = {
-                        'file_path': a_file_path, 
-                        'line_number': text_line_number, 
+                        'file_path': a_file_path,
+                        'line_number': text_line_number,
                         'node_type': node_type,
                         'text_type': 'text'}
                 elif tooltip_text != '':
                     a_results[tooltip_text] = {
-                        'file_path': a_file_path, 
-                        'line_number': tooltip_text_line_number, 
+                        'file_path': a_file_path,
+                        'line_number': tooltip_text_line_number,
                         'node_type': node_type,
                         'text_type': 'tooltip_text'}
             node_type = ''
@@ -98,13 +101,16 @@ def _scan_tscn_file(a_file_path, a_results, a_files_list):
 def generate_mo():
     file_path = os.path.join(os.path.dirname(__file__))
     for po_file in os.listdir(os.path.join(file_path, 'po_files')):
-        if not po_file.endswith('.po'): continue
-        mo_file = os.path.join(file_path, '../src/translations/', po_file[:-3] + '.mo')
+        if not po_file.endswith('.po'):
+            continue
+        mo_file = os.path.join(
+            file_path, '../src/translations/', po_file[:-3] + '.mo')
         os.system(f'msgfmt -o {mo_file} {file_path}/po_files/{po_file}')
 
 
 def create_new_locale():
-    # Ask for locale (Use toolbox and check against possible locales to see if correct)
+    # Ask for locale (Use toolbox and check against possible locales
+    # to see if correct)
     # Generate new po file with POT file
     print('not implemented yet')
 
@@ -113,7 +119,7 @@ def menu():
     match toolbox.get_input_choice('Localization menu', [
         'Generate POT',
         'Generate *.mo files',
-        'Add new locale']):
+            'Add new locale']):
         case 0: generate_pot()
         case 1: generate_mo()
         case 2: create_new_locale()
