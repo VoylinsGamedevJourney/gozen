882754fa18b6351eecd49ad758c74d8b8d66fab8
voylin <0voylin0@gmail.com>
2024-09-01 14:53:19 +0900

    Adding/Updating classes

---
+++ src/classes/loadable.gd
@@ -0,0 +1,15 @@
+class_name Loadable
+extends Node
+
+
+var info_text: String
+var function: Callable
+var delay: float
+
+
+func _init(a_info_text: String, a_func: Callable, a_delay: float) -> void:
+	info_text = a_info_text
+	function = a_func
+	delay = a_delay
+	
+
---
+++ src/classes/recent_project_data.gd
@@ -0,0 +1,8 @@
+class_name RecentProjectData
+extends Node
+
+var id: int
+var title: String
+var path: String
+var creation_date: String
+var last_edited: String
