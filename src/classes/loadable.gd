a2ceadeb212900dc8348a2abba6559ea64ae413f
voylin <0voylin0@gmail.com>
2024-09-07 10:59:37 +0900

    Improved loading scene

--- src/classes/loadable.gd
+++ src/classes/loadable.gd
@@ -7,9 +7,8 @@ var function: Callable
 var delay: float
 
 
-func _init(a_info_text: String, a_func: Callable, a_delay: float) -> void:
+func _init(a_info_text: String, a_func: Callable, a_delay: float = 0) -> void:
 	info_text = a_info_text
 	function = a_func
 	delay = a_delay
-	
 
--- src/scenes/loading_screen/loading_scene.gd
+++ src/scenes/loading_screen/loading_scene.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const MIN_TIME: int = 3
+
 
 @export var loading_text_label: Label
 @export var version_label: Label
@@ -7,15 +9,31 @@ extends Control
 
 
 func _ready() -> void:
+	var l_total_time: float = 0
 	version_label.text = "Version: %s " % ProjectSettings.get_setting("application/config/version")
 
-	for l_loadable: Loadable in GozenServer.loadables:
-		loading_text_label.text = ": %s..." % l_loadable.info_text
+	for l_loadable: Loadable in GoZenServer.loadables as Array[Loadable]:
+		loading_text_label.text = "%s..." % l_loadable.info_text
 		loading_text_label.tooltip_text = l_loadable.info_text
+		await RenderingServer.frame_pre_draw
 		await l_loadable.function.call()
-		timer.wait_time = l_loadable.delay
-		timer.start()
+
+		if l_loadable.delay == 0:
+			l_total_time += 0.1
+			timer.start(0.1)
+		else:
+			l_total_time += l_loadable.delay
+			timer.start(l_loadable.delay)
+
+		l_total_time += timer.wait_time
 		await timer.timeout
+	
+	loading_text_label.text = "Finalizing ..."
+	loading_text_label.tooltip_text = "Finalizing"
+	await RenderingServer.frame_pre_draw
+	
+	timer.start(MIN_TIME - l_total_time if l_total_time < MIN_TIME else 1.)
+	await timer.timeout
 
 	get_viewport().get_window().unresizable = false
 	self.visible = false
@@ -24,15 +42,21 @@ func _ready() -> void:
 	# Check for tiling window managers
 	if OS.get_name() == "Linux":
 		var l_reply: Array = []
-		print(OS.execute("echo", ["$XDG_CURRENT_DESKTOP"], l_reply))
+		if OS.execute("echo", ["$XDG_CURRENT_DESKTOP"], l_reply):
+			printerr("Something went wrong getting XDG_CURRENT_DESKTOP")
 		var l_wm: String = l_reply[0]
 		match l_wm.trim_suffix("\n"):
 			"i3":
+				get_window().grab_focus()
 				if OS.execute("i3-msg", ["floating", "disable"]):
 					printerr("Error occured when making non floating on i3!")
 			_: print("Not a tiling window manager")
 
-	# TODO: on end load main UI
+	GoZenServer.loadables = [] # Cleanup of memory
+
+	if get_tree().change_scene_to_file("res://scenes/main_screen/main_scene.tscn"):
+		printerr("Couldn't load main scene!")
+		get_tree().quit(-2)
 
 
 func _on_meta_clicked(a_meta: String) -> void:
