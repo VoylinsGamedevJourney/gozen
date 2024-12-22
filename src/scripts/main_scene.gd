4fb96a034d96f3735e7dcfaf60660df518b9530c
voylin <0voylin0@gmail.com>
2024-12-22 13:39:50 +0900

    Menu bar script

--- src/scripts/main_scene.gd
+++ src/scripts/main_scene.gd
@@ -7,17 +7,3 @@ func _ready() -> void:
 			# TODO: Load project with the path found
 			break
 
-
-func _on_project_id_pressed(a_id: int) -> void: ## Menu bar popup button
-	match a_id:
-		0: Project.save() # Save current project
-		1: Project.save("") # Save current project to new location
-		2: Project.load("") # Load project
-		3: pass # TODO: Add a load recent projects dropdown
-
-
-func _on_help_id_pressed(a_id: int) -> void: ## Menu bar popup button
-	match a_id:
-		0: Utils.open_url("https://voylin.com/projects/gozen/")
-		1: Utils.open_url("https://ko-fi.com/voylin")
-
