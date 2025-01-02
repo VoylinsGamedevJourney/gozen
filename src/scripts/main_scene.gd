f4ded198763b321948ee3911ef03d878d5e3c6b7
voylin <0voylin0@gmail.com>
2025-01-03 08:08:10 +0900

    Add Window movement

--- src/scripts/main_scene.gd
+++ src/scripts/main_scene.gd
@@ -4,6 +4,9 @@ extends Control
 var resizing: bool = false
 var new_size: Vector2i = Vector2i.ZERO
 
+var moving: bool = false
+var move_offset: Vector2 = Vector2.ZERO
+
 
 
 func _ready() -> void:
@@ -35,4 +38,16 @@ func _on_resize_handle_gui_input(a_event: InputEvent, a_handle: int) -> void:
 		# Bottom + corner handling
 		if a_handle & 2:
 			new_size.y = int(get_global_mouse_position().y)
+
+
+func _on_menu_bar_panel_gui_input(a_event: InputEvent) -> void:
+	if a_event is InputEventMouseButton and a_event.get("button_index") == 1:
+		if !moving and a_event.is_pressed():
+			moving = true
+			move_offset = get_viewport().get_mouse_position()
+		elif !a_event.is_pressed():
+			moving = false
+
+	if moving:
+		get_window().position += Vector2i(get_global_mouse_position() - move_offset)
 
