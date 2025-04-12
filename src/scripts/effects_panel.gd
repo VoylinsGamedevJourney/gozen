633b6869f322e60b2dff03bf449f75b27937a095
voylin <0voylin0@gmail.com>
2025-04-13 08:03:32 +0900

    Effects + View panel scripts init

---
+++ src/scripts/effects_panel.gd
@@ -0,0 +1,24 @@
+extends PanelContainer
+
+# TODO: Text effects should be on top of the visual effects VBox.
+# Make these effects invisible for non-text clips.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_video_effects_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_audio_effects_button_pressed() -> void:
+	pass # Replace with function body.
+
+
---
+++ src/scripts/view_panel.gd
@@ -0,0 +1,28 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_skip_prev_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_pause_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_skip_next_button_pressed() -> void:
+	pass # Replace with function body.
+
