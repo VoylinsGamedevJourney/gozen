c18a895f03ca683b37938d70d1c3c598745e27d1
voylin <voylinslife@gmail.com>
2026-01-10 10:47:00 +0900

    Add transform effect

--- src/scripts/classes/clip_data.gd
+++ src/scripts/classes/clip_data.gd
@@ -17,19 +17,7 @@ var begin: int = 0 # Only for video and audio files
 var effects_video: Array[VisualEffect]
 var effects_audio: Array[AudioEffect]
 
-var transform: TransformEffect # Only for clips with visuals
-
 
 
 func _to_string() -> String:
 	return "<clip:%s>" % id
-
-
-
-class TransformEffect:
-	var position: Dictionary[int, Vector2i] = { 0: Vector2i.ZERO }
-	var size: Dictionary[int, Vector2i] = { 0: Vector2i.ZERO }
-	var scale: Dictionary[int, float] = { 0: 100 }
-	var rotation: Dictionary[int, float] = { 0: 0 }
-	var alpha: Dictionary[int, float] = { 0: 1.0 }
-	var pivot: Dictionary[int, Vector2i] = { 0: Vector2i.ZERO }
