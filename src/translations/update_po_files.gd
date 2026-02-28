6642e25e8e87f32e014947571a0a891103a17cea
voylin <voylinslife@gmail.com>
2026-02-28 15:46:32 +0900

    Update po files updater

--- src/translations/update_po_files.gd
+++ src/translations/update_po_files.gd
@@ -17,21 +17,23 @@ func _run() -> void:
 	while file_name != "":
 		if not dir.current_is_dir() and file_name.get_extension() == "po":
 			var po_path: String = base_dir.path_join(file_name)
-			_merge_po_file(po_path, pot_path)
+			if !_merge_po_file(po_path, pot_path):
+				print("Updating PO files failed!")
+				return
 		file_name = dir.get_next()
 
 	EditorInterface.get_resource_filesystem().scan()
 	print("Updating PO files complete!")
 
 
-func _merge_po_file(po_path: String, pot_path: String) -> void:
+func _merge_po_file(po_path: String, pot_path: String) -> bool:
 	var arguments: PackedStringArray = ["--update", "--backup=none", po_path, pot_path]
 	var output: PackedStringArray = []
 
-	print("Updating: ", po_path.get_file(), " ...")
+	print("Updating: ", po_path.get_file())
 	if OS.execute("msgmerge", arguments, output, true) != 0:
 		printerr("Failed to update ", po_path.get_file())
 		printerr("Is 'msgmerge' in your system PATH?")
 		printerr("Output: ", output)
-	else:
-		print("Success\n---")
+		return false
+	return true
