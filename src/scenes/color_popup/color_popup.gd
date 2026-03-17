abb1829da6b1e76bfe93951afd928fefd2a5b1c6
voylin <voylinslife@gmail.com>
2026-03-17 18:40:44 +0900

    Color panel fix

--- src/scenes/color_popup/color_popup.gd
+++ src/scenes/color_popup/color_popup.gd
@@ -1,11 +1,12 @@
-extends PanelContainer
+extends Control
 
 @export var color_picker: ColorPicker
 
 
 func _on_create_color_pressed() -> void:
 	Project.files.add(["temp://color#" + color_picker.color.to_html()])
-	queue_free()
+	PopupManager.close(PopupManager.COLOR)
 
 
-func _on_cancel_button_pressed() -> void: queue_free()
+func _on_cancel_button_pressed() -> void:
+	PopupManager.close(PopupManager.COLOR)
