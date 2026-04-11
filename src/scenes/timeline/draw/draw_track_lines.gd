9dbfce5bfdf5b8ed1d5c2fc71cc7c122935a6cab
voylin <voylinslife@gmail.com>
2026-04-11 10:50:47 +0900

    Add line to bottom of track lines

--- src/scenes/timeline/draw/draw_track_lines.gd
+++ src/scenes/timeline/draw/draw_track_lines.gd
@@ -8,7 +8,7 @@ func _draw() -> void:
 	var scroll_container: ScrollContainer = get_parent().get_parent()
 	var scroll_start: float = Timeline.scroll_x
 	var scroll_end: float = scroll_start + scroll_container.size.x
-	for i: int in TrackLogic.tracks.size() - 1:
+	for i: int in TrackLogic.tracks.size():
 		var y_pos: float = Timeline.track_total_size * (i + 1)
 		draw_dashed_line(
 				Vector2(scroll_start, y_pos), Vector2(scroll_end, y_pos),
