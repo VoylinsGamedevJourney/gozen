a6a915c0db7c75ab96ef7ce7ccecdffb29c2b8e3
voylin <voylinslife@gmail.com>
2026-06-20 11:00:59 +0900

    Updating to 4.7

--- src/scenes/timeline/timeline_scroll.gd
+++ src/scenes/timeline/timeline_scroll.gd
@@ -19,35 +19,6 @@ func _ready() -> void:
 	@warning_ignore_restore("return_value_discarded")
 
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var event_mouse_button: InputEventMouseButton = event
-		if !event_mouse_button.pressed:
-			return
-
-		# We need to change the scroll direction of this node.
-		# TODO: Implemented as a PR for Godot (#115545) Got merged, still need 4.7 to actually use it.
-		var h_step: float = scroll_horizontal_custom_step
-		var v_step: int = floori(scroll_vertical_custom_step)
-
-		if event.is_action_pressed("scroll_left", false, true):
-			scroll_horizontal -= floori(h_step * maxf(Timeline.zoom, 2.0))
-			accept_event()
-			get_h_scroll_bar().scrolling.emit()
-		elif event.is_action_pressed("scroll_right", false, true):
-			scroll_horizontal += floori(h_step * maxf(Timeline.zoom, 2.0))
-			accept_event()
-			get_h_scroll_bar().scrolling.emit()
-		elif event.is_action_pressed("scroll_up", false, true):
-			scroll_vertical -= v_step
-			accept_event()
-			get_h_scroll_bar().scrolling.emit()
-		elif event.is_action_pressed("scroll_down", false, true):
-			scroll_vertical += v_step
-			accept_event()
-			get_h_scroll_bar().scrolling.emit()
-
-
 func _on_timestamp_scrolling(value: float) -> void:
 	if self.scroll_horizontal != int(value):
 		self.scroll_horizontal = int(value)
