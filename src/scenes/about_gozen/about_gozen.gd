e9c697fb42c49e8e93655d068f1cdb84565bd189
voylin <0voylin0@gmail.com>
2026-08-03 22:00:10 +0900

    Update about GoZen popup

--- src/scenes/about_gozen/about_gozen.gd
+++ src/scenes/about_gozen/about_gozen.gd
@@ -1,49 +1,35 @@
 extends PanelContainer
-# TODO: Maybe add a tabbed interface.
-# General tab: Logo, version (With a new version available indicator), short description, and links (to site, github, and socials)
-# Credits: A list of Sponsors and main contributors (Requirement would be 10 PR's I guess)
-# System Info: Technical details for debugging (OS, GPU, RAM) (Add button to copy debug info)
-# Licenses: Show the GPLv3 license for GoZen (and the third party licenses?)
 
 const URL: String = "[color=#A718F1][url=URL]URL[/url][/color]"
 const PROJECT_SETTING_VERSION: String = "application/config/version"
 
 
-@export var version_label: Label
-@export var links_label: RichTextLabel
+@export var line_edit_version: LineEdit
+@export var rich_text_label_website: RichTextLabel
+@export var rich_text_label_source_code: RichTextLabel
+@export var rich_text_label_discord: RichTextLabel
+@export var rich_text_label_youtube: RichTextLabel
+@export var rich_text_label_support: RichTextLabel
+
+@export var rich_text_label_made_by: RichTextLabel
+@export var rich_text_label_license: RichTextLabel
+
 
 
 func _ready() -> void:
-	_set_version_label()
-	_set_links_label()
+	line_edit_version.text = "%s%s" % [
+			ProjectSettings.get_setting(PROJECT_SETTING_VERSION),
+			"-debug" if OS.is_debug_build() else "-debug"]
 
+	rich_text_label_website.text = "[url=URL]URL".replace("URL", ProjectSettings.get_setting("urls/site") as String)
+	rich_text_label_source_code.text = "[url=URL]URL".replace("URL", ProjectSettings.get_setting("urls/source_code") as String)
+	rich_text_label_discord.text = "[url=URL]URL".replace("URL", ProjectSettings.get_setting("urls/discord") as String)
+	rich_text_label_support.text = "[url=URL]URL".replace("URL", ProjectSettings.get_setting("urls/support") as String)
+	rich_text_label_youtube.text = "[url=URL]URL".replace("URL", ProjectSettings.get_setting("urls/youtube_channel") as String)
 
-func _set_version_label() -> void:
-	var version_string: String = tr("GoZen version") + ": "
-	version_string += ProjectSettings.get_setting(PROJECT_SETTING_VERSION)
-
-	if OS.is_debug_build():
-		version_string += "-debug"
-	version_label.text += version_string
-
-
-func _set_links_label() -> void:
-	var site_url: String = ProjectSettings.get_setting("urls/site")
-	var github_url: String = ProjectSettings.get_setting("urls/github")
-	var discord_url: String = ProjectSettings.get_setting("urls/discord")
-	var support_url: String = ProjectSettings.get_setting("urls/support")
-	var youtube_url: String = ProjectSettings.get_setting("urls/youtube_channel")
-
-	var lines: PackedStringArray = [
-			tr("Website") + ": " + URL.replace("URL", site_url),
-			tr("GitHub repo") + ": " + URL.replace("URL", github_url),
-			tr("Discord") + ": " + URL.replace("URL", discord_url),
-			tr("Support GoZen") + ": " + URL.replace("URL", support_url),
-			"",
-			tr("Made by [color=#A718F1][url=URL]Voylin's Gamedev Journey[/url][/color]! ;)").replace("URL", youtube_url)
-	]
-	for line: String in lines:
-		links_label.text += line + "\n"
+	rich_text_label_made_by.text = "[center]" + tr("Made by [url=%s]Voylin's Gamedev Journey[/url]!") % [
+			"https://youtube.com/@VoylinsGamedevJourney"]
+	rich_text_label_license.text = "[center]" + tr("License") + ": [url=%s]GPLv3" % ProjectSettings.get_setting("urls/license")
 
 
 func _input(event: InputEvent) -> void:
