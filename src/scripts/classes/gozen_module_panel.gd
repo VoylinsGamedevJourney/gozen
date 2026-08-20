91d22ce572e4c60de15794b63ea2f2c104ee3f17
voylin <0voylin0@gmail.com>
2026-08-20 19:55:27 +0900

    Init module management system

---
+++ src/scripts/classes/gozen_module_panel.gd
@@ -0,0 +1,12 @@
+class_name GoZenModulePanel
+extends Resource
+## NOTE: Give the folder name in "res://modules/" a unique name so it doesn't
+## 		 interfere with other modules!
+
+@export_group("Module info")
+@export var name: String = ""
+@export var description: String = ""
+@export var author: String = ""
+@export var version: String = "1.0"
+
+# TODO: Implement this
